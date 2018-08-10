7acce4ada12bf1722cf3fcc93397680a5fb6a59e
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-10 10:22:59 -0700

    add var values in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  var ruby = 0
-  var react = 0
-  var cSharp = 0
+  $("button#big-button").click(function(event) {
+    event.preventDefault();
+    var ruby = 0
+    var react = 0
+    var cSharp = 0
 
-
+    var question1 = $("#qCompanySize").val();
+    var question2 = $("#qFrontOrBack").val();
+    var question3 = $("#qEasyReliable").val();
+    var question4 = $("#qHtmlJavascript").val();
+    var question5 = $("#qUserBusiness").val();
+    var question6 = $("#qWhichCompany").val();
+  });
 });
