016bb5ba9ac9d8736cb812ed6cf77ddd420a510d
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-18 19:04:56 -0700

    add semicolons, clarify variable names

--- js/scripts.js
+++ js/scripts.js
@@ -4,117 +4,120 @@ $(document).ready(function() {
     $(".winner-block").hide();
     $(".tiebreaker-block").hide();
 
-    var ruby = 0
-    var react = 0
-    var cSharp = 0
+    var ruby = 0;
+    var react = 0;
+    var cSharp = 0;
+    var companySize = $("#qCompanySize").val();
+    var frontOrBack = $("#qFrontOrBack").val();
+    var easyReliable = $("#qEasyReliable").val();
+    var htmlJavascript = $("#qHtmlJavascript").val();
+    var userBusiness = $("#qUserBusiness").val();
+    var whichCompany = $("#qWhichCompany").val();
+    var tRuby = $("select#qTiebreak").children("option#tRuby");
+    var tCSharp = $("select#qTiebreak").children("option#tCSharp");
+    var tReact = $("select#qTiebreak").children("option#tReact");
 
-    var question1 = $("#qCompanySize").val();
-    var question2 = $("#qFrontOrBack").val();
-    var question3 = $("#qEasyReliable").val();
-    var question4 = $("#qHtmlJavascript").val();
-    var question5 = $("#qUserBusiness").val();
-    var question6 = $("#qWhichCompany").val();
 
-    if (question1 === "Big Company") {
+    if (companySize === "Big Company") {
       cSharp ++;
-    } else if (question1 === "Small Company") {
+    } else if (companySize === "Small Company") {
       ruby ++;
     } else {
       react ++;
     }
-    console.log(ruby, react, cSharp)
 
-    if (question2 === "Frontend") {
+    if (frontOrBack === "Frontend") {
       react ++;
     } else {
       ruby ++;
       cSharp ++;
     }
-    console.log(ruby, react, cSharp);
 
-    if (question3 === "Ease of Use") {
+    if (easyReliable === "Ease of Use") {
       react ++;
       ruby ++;
     } else {
       cSharp ++;
     }
-    console.log(ruby, react, cSharp);
 
-    if (question4 === "HTML") {
+    if (htmlJavascript === "HTML") {
       cSharp ++;
       ruby ++;
     } else {
       react ++;
     }
-    console.log(ruby, react, cSharp);
 
-    if (question5 === "Individual Users") {
+    if (userBusiness === "Individual Users") {
       react ++;
-    } else if (question5 === "Businesses") {
+    } else if (userBusiness === "Businesses") {
       cSharp ++;
     } else {
       ruby ++;
     }
-    console.log(ruby, react, cSharp);
 
-    if (question6 === "Miscrosoft") {
+    if (whichCompany === "Miscrosoft") {
       cSharp ++;
-    } else if (question6 === "Netflix") {
+    } else if (whichCompany === "Netflix") {
       react ++;
-    } else if (question6 === "Airbnb") {
+    } else if (whichCompany === "Airbnb") {
       ruby ++;
-    } else if (question6 === "Dropbox") {
+    } else if (whichCompany === "Dropbox") {
       react ++;
-    } else if (question6 === "Blizzard") {
+    } else if (whichCompany === "Blizzard") {
       cSharp ++;
-    } else if (question6 === "Soundcloud") {
+    } else if (whichCompany === "Soundcloud") {
       ruby ++;
-    } else if (question6 === "Instagram") {
+    } else if (whichCompany === "Instagram") {
       react ++;
     } else {
       ruby ++;
     }
-    console.log("ruby", "react", "cSharp");
-    console.log(ruby, react, cSharp);
 
-    if (ruby === react) {
-      $(".tiebreaker-block").show();
-      $("select#qTiebreak").children("option#tCSharp").remove();
-      $("button#tie-button").click(function(event) {
-        event.preventDefault();
-        var question7 = $("#qTiebreak").val();
-        if (question7 === "Slow Runtime Speed") {
-          ruby += 0.5;
-        } else {
-          react += 0.5;
-        }
-      });
-    } if (cSharp === react) {
-      $(".tiebreaker-block").show();
-      $("select#qTiebreak").children("option#tRuby").remove();
-      $("button#tie-button").click(function(event) {
-        event.preventDefault();
-        var question7 = $("#qTiebreak").val();
-        if (question7 === "Hard to Learn") {
-          cSharp += 0.5;
-        } else {
-          react += 0.5;
-        }
-      });
-    } if (ruby === cSharp) {
-      $(".tiebreaker-block").show();
-      $("select#qTiebreak").children("option#tReact").remove();
-      $("button#tie-button").click(function(event) {
-        event.preventDefault();
-        var question7 = $("#qTiebreak").val();
-        if (question7 === "Slow Runtime Speed") {
-          ruby += 0.5;
-        } else {
-          cSharp += 0.5;
-        }
-      });
+    tRuby.show();
+    tCSharp.show();
+    tReact.show();
+    if ((cSharp < (react && ruby)) || (react < (cSharp && ruby)) || (ruby < (cSharp && react))) {
+
+      if (ruby === react) {
+        $(".tiebreaker-block").show();
+        tCSharp.hide();
+        $("button#tie-button").click(function(event) {
+          event.preventDefault();
+          var tiebreak = $("#qTiebreak").val();
+          if (tiebreak === "Slow Runtime Speed") {
+            ruby += 0.5;
+          } else {
+            react += 0.5;
+          }
+        });
+      } else if (cSharp === react) {
+        $(".tiebreaker-block").show();
+        tRuby.hide();
+        $("button#tie-button").click(function(event) {
+          event.preventDefault();
+          var tiebreak = $("#qTiebreak").val();
+          if (tiebreak === "Hard to Learn") {
+            cSharp += 0.5;
+          } else {
+            react += 0.5;
+          }
+        });
+      } else if (ruby === cSharp) {
+        $(".tiebreaker-block").show();
+        tReact.hide();
+        $("button#tie-button").click(function(event) {
+          event.preventDefault();
+          var tiebreak = $("#qTiebreak").val();
+          if (tiebreak === "Slow Runtime Speed") {
+            ruby += 0.5;
+          } else {
+            cSharp += 0.5;
+          }
+        });
+      } else {
+        $(".tiebreaker-block").hide();
+      }
     }
-
     if ($(".tiebreaker-block").is(':visible')) {
       $("button#tie-button").click(function(event) {
         event.preventDefault();
@@ -125,16 +128,16 @@ $(document).ready(function() {
           $("#winner-button").html("Ruby");
           $(".winner-block").show();
           $(".tiebreaker-block").hide();
-        } else if (react >cSharp && react > ruby) {
+        } else if (react > cSharp && react > ruby) {
           $("#winner-pic").attr("src", "img/react.png");
-          $("#winner-link").attr("href", "https://reactjs.org/")
+          $("#winner-link").attr("href", "https://reactjs.org/");
           $("#winner-name").html("React");
           $("#winner-button").html("React");
           $(".winner-block").show();
           $(".tiebreaker-block").hide();
-        } else {
+        } else if (cSharp > react && cSharp > ruby) {
           $("#winner-pic").attr("src", "img/csharp.jpg");
-          $("#winner-link").attr("href", "https://docs.microsoft.com/en-us/dotnet/csharp/")
+          $("#winner-link").attr("href", "https://docs.microsoft.com/en-us/dotnet/csharp/");
           $("#winner-name").html("C#");
           $("#winner-button").html("C#");
           $(".winner-block").show();
@@ -145,19 +148,19 @@ $(document).ready(function() {
 
       if (ruby > cSharp && ruby > react) {
         $("#winner-pic").attr("src", "img/ruby.png");
-        $("#winner-link").attr("href", "https://www.ruby-lang.org/en/")
+        $("#winner-link").attr("href", "https://www.ruby-lang.org/en/");
         $("#winner-name").html("Ruby");
         $("#winner-button").html("Ruby");
         $(".winner-block").show();
-      } else if (react >cSharp && react > ruby) {
+      } else if (react > cSharp && react > ruby) {
         $("#winner-pic").attr("src", "img/react.png");
-        $("#winner-link").attr("href", "https://reactjs.org/")
+        $("#winner-link").attr("href", "https://reactjs.org/");
         $("#winner-name").html("React");
         $("#winner-button").html("React");
         $(".winner-block").show();
-      } else {
+      } else if (cSharp > react && cSharp > ruby) {
         $("#winner-pic").attr("src", "img/csharp.jpg");
-        $("#winner-link").attr("href", "https://docs.microsoft.com/en-us/dotnet/csharp/")
+        $("#winner-link").attr("href", "https://docs.microsoft.com/en-us/dotnet/csharp/");
         $("#winner-name").html("C#");
         $("#winner-button").html("C#");
         $(".winner-block").show();
